83d5439bf714668e981d3467d2385c8e0f27b124
Venkatesh Mani Selvaraj <vmselvaraj@outlook.com>
2016-12-01 16:00:24 +0530

    Add project files.

---
+++ ExpressionSolver/ExpressionSolver.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beyond.ExpressionSolver
+{
+    public class ExpressionSolver
+    {
+        public ExpressionSolver()
+        {
+
+        }
+    }
+}
---
+++ ExpressionSolverTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ExpressionSolverTest
+{
+    [TestClass]
+    public class ExpressionSolverTest
+    {
+        [TestMethod]
+        public void Solve()
+        {
+
+        }
+    }
+}
