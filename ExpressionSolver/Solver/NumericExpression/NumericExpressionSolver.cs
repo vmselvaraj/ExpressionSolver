e956922fbcbb0020c068cf83d51ea5a311fe3c17
Venkatesh Mani Selvaraj <vmselvaraj@outlook.com>
2016-12-02 15:54:42 +0530

    Added Numeric Expression Solver

--- ExpressionSolver/Solver/NumericExpression/NumericExpressionSolver.cs
+++ ExpressionSolver/Solver/NumericExpression/NumericExpressionSolver.cs
@@ -6,10 +6,108 @@ using System.Threading.Tasks;
 
 namespace Beyond.ExpressionSolver
 {
-    public class NumericExpression
+    public static class NumericExpression
     {
-        public NumericExpression()
+        private static bool IsNumeric(string s, out double Number)
         {
+            double Parsed = -1;
+            Number = -1;
+            if (double.TryParse(s, out Parsed))
+            {
+                Number = Parsed;
+                return true;
+            }
+            return false;
+        }
+
+        private static bool hasPrecedence(string op1, string op2)
+        {
+            if (op2 == "(" || op2 == ")" || op2 == "^")
+                return false;
+            if ((op1 == "*" || op1 == "/") && (op2 == "+" || op2 == "-"))
+                return false;
+            else
+                return true;
+        }
+
+
+        private static double PerformOperations(string op, double b, double a)
+        {
+            switch (op)
+            {
+                case "+":
+                    return a + b;
+                case "-":
+                    return a - b;
+                case "*":
+                    return a * b;
+                case "/":
+                    if (b == 0)
+                        throw new DivideByZeroException();
+                    return a / b;
+                case "^":
+                    return Math.Pow(a, b);
+            }
+            return 0;
+        }
+
+        public static double Evaluate(string text)
+        {
+            if (text.StartsWith("-") || text.StartsWith("+"))
+                text = "0" + text;
+
+            char[] delimiterChars = { '+', '-', '*', '/', '^', '(', ')' };
+
+            string[] words = text.Split(delimiterChars);
+            List<string> Expression = words.ToList();
+            Stack<double> Values = new Stack<double>();
+            Stack<string> Operands = new Stack<string>();
+            int count = 1;
+            double Number;
+
+            foreach (char c in text)
+            {
+                if (delimiterChars.Contains(c))
+                {
+                    Expression.Insert(count, c.ToString());
+                    count = count + 2;
+                }
+            }
+            Console.WriteLine();
+
+            foreach (string s in Expression)
+            {
+
+                if (string.Compare(s, "(") == 0)
+                    Operands.Push(s);
+                else if (string.Compare(s, ")") == 0)
+                {
+                    while (Operands.Peek() != "(")
+                        Values.Push(PerformOperations(Operands.Pop(), Values.Pop(), Values.Pop()));
+                    Operands.Pop();
+                }
+                else if (s == "^")
+                {
+                    // Values.Push(PerformOperations(Operands.Pop(), Values.Pop(), Values.Pop()));
+                    Operands.Push(s);
+                }
+
+
+                else if (s == "+" || s == "-" || s == "*" || s == "/" || s == "^")
+                {
+                    while (Operands.Count != 0 && hasPrecedence(s, Operands.Peek()))
+                        Values.Push(PerformOperations(Operands.Pop(), Values.Pop(), Values.Pop()));
+                    Operands.Push(s);
+                }
+                else if (IsNumeric(s, out Number))
+                    Values.Push(Number);
+
+            }
+
+            while (Operands.Count != 0)
+                Values.Push(PerformOperations(Operands.Pop(), Values.Pop(), Values.Pop()));
+
+            return Math.Round(Values.Pop(), 3);
 
         }
     }
---
+++ ExpressionSolverTests/Solver/NumericExpression/NumericExpressionTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Beyond.ExpressionSolver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beyond.ExpressionSolver.Tests
+{
+    [TestClass()]
+    public class NumericExpressionTests
+    {
+        [TestMethod()]
+        public void SimpleExpressions()
+        {
+            Assert.AreEqual(1, NumericExpression.Evaluate("1/1"));
+            Assert.AreEqual(0.5d, NumericExpression.Evaluate("1/2"));
+            Assert.AreEqual(1, NumericExpression.Evaluate("1*1"));
+            Assert.AreEqual(2, NumericExpression.Evaluate("1+1"));
+            Assert.AreEqual(0, NumericExpression.Evaluate("1-1"));
+            Assert.AreEqual(0, NumericExpression.Evaluate("-1+1"));
+            Assert.AreEqual(1, NumericExpression.Evaluate("1^1"));
+            Assert.AreEqual(1, NumericExpression.Evaluate("-1^2"));
+            Assert.AreEqual(3, NumericExpression.Evaluate("-1+2*2"));
+            Assert.AreEqual(-3, NumericExpression.Evaluate("-2*2+1"));
+            Assert.AreEqual(49, NumericExpression.Evaluate("(3 + 4)^2"));
+            Assert.AreEqual(0.25d, NumericExpression.Evaluate("2^-2"));
+            Assert.AreEqual(0.25d, NumericExpression.Evaluate("1/(2^2)"));
+            Assert.AreEqual(4.25d, NumericExpression.Evaluate("1/4+2*2"));
+            Assert.AreEqual(0.25d, NumericExpression.Evaluate("1/4+2*2-2^2(2/2)"));
+            Assert.AreEqual(1, NumericExpression.Evaluate("-1 * -1"));
+            Assert.AreEqual(16, NumericExpression.Evaluate("(2^2)2^2"));
+            Assert.AreEqual(0, NumericExpression.Evaluate("1^0"));
+        }
+
+
+        [TestMethod()]
+        public void FloatingPoint()
+        {
+            Assert.AreEqual(0.01d, NumericExpression.Evaluate("0.1^2"));
+            Assert.AreEqual(2, NumericExpression.Evaluate("4^0.5"));
+            Assert.AreEqual(0.12d, NumericExpression.Evaluate("0.1 + 0.2 * 0.1"));
+            Assert.AreEqual(0.001d, NumericExpression.Evaluate("-0.1 * -0.1"));
+            Assert.AreEqual(10, NumericExpression.Evaluate("1/0.1"));
+            Assert.AreEqual(0.1d, NumericExpression.Evaluate("-0.1 + 0.2 * 0.01/0.1^2"));
+
+        }
+    }
+}
